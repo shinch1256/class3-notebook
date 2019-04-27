ab49dd56eaa7eb98570b2bac4555d396882f8487
Jason <cheng-hung.hsin@mail.mcgill.ca>
2019-04-27 12:08:14 -0400

    fizzebuzz file added

---
+++ FizzBuzz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+for  i in range (101):
+    if (i % 15 == 0):
+        print(f'{i} FizzBuzz')
+    elif (i % 3 == 0):
+        print(f'{i} Fizz')
+    elif(i % 5 == 0):
+        print(f'{i} Buzz')
+    else:
+        print(i)
+            
+   
+
+
+# In[ ]:
+
+
+
+
